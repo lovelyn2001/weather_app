8d4a205559f114e7c18685c32e782ad47fad624f
lovelyn2001 <lovelynikwuka2001@gmail.com>
2025-07-21 16:52:22 +0100

    updated the functionality to use the location from the user's device

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -9,15 +11,46 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
   initState() {
     super.initState();
     getLocation();
   }
 
-  void getLocation() async {
+  Future<void> getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+
+    setState(() {
+      latitude = location.latitude;
+      longitude = location.longitude;
+    });
     print('Latitude: ${location.latitude}, Longitude: ${location.longitude}');
+  }
+
+  void getWeatherData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=785ec99797a3c2373669d88ddfcccc6e',
+      ),
+    );
+    if (response.statusCode == 200) {
+      String data = response.body;
+      double temperature;
+      int condition;
+      String cityName;
+      dynamic decodedData = jsonDecode(data);
+
+      temperature = decodedData['main']['temp'];
+      print('Temperature: $temperature');
+      condition = decodedData['weather'][0]['id'];
+      print('Condition: $condition');
+      cityName = decodedData['name'];
+      print('City Name: $cityName');
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -25,8 +58,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            getLocation();
+          onPressed: () async {
+            await getLocation();
+            getWeatherData();
           },
           child: Text('Get Location'),
         ),
